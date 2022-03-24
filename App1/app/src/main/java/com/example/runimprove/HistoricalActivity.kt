8ad6b4806a3b62c86164748111ec641a8086c21e
AlejandroEscalona <1alejandroescalona@gmail.com>
2022-03-24 17:18:18 +0100

    Imagen home, y avances home

---
+++ App1/app/src/main/java/com/example/runimprove/HistoricalActivity.kt
@@ -0,0 +1,11 @@
+package com.example.runimprove
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HistoricalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historical)
+    }
+}
--- App1/app/src/main/java/com/example/runimprove/MainActivity.kt
+++ App1/app/src/main/java/com/example/runimprove/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.runimprove
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.runimprove.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
-}
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return false
+    }
+
+    fun OnClickButton(view: View){
+
+        when(view.id){
+            R.id.btnTraining -> {
+                val intent = Intent(this,TrainingActivity::class.java)
+                launchIntent(intent)
+            }
+
+            R.id.btnHistorical -> {
+                val intent = Intent(this,HistoricalActivity::class.java)
+                launchIntent(intent)
+            }
+        }
+    }
+
+    private fun launchIntent(intent: Intent){
+        if(intent.resolveActivity(packageManager) != null){
+            startActivity(intent)
+        } else {
+            Toast.makeText(this,"No se encontro una app compatible", Toast.LENGTH_SHORT)
+        }
+    }
+
+    }

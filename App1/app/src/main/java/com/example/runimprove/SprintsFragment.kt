f0b44acece3a24b4d5e0773ff1cff8bdfde465f8
AlejandroEscalona <1alejandroescalona@gmail.com>
2022-03-31 18:55:04 +0200

    CountDown in Sprint

--- App1/app/src/main/java/com/example/runimprove/SprintsFragment.kt
+++ App1/app/src/main/java/com/example/runimprove/SprintsFragment.kt
@@ -1,11 +1,15 @@
 package com.example.runimprove
 
+import android.media.RingtoneManager
+import android.media.RingtoneManager.getRingtone
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import com.example.runimprove.databinding.FragmentSprintsBinding
 
@@ -13,6 +17,7 @@ import com.example.runimprove.databinding.FragmentSprintsBinding
 class SprintsFragment : Fragment() {
 
     private lateinit var mBinding : FragmentSprintsBinding
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
@@ -29,6 +34,22 @@ class SprintsFragment : Fragment() {
 
         val enlace: TextView = mBinding.twEnalce
         enlace.movementMethod = LinkMovementMethod.getInstance()
+
+        mBinding.btnPlay.setOnClickListener {
+            object : CountDownTimer(10000, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    mBinding.tvCuentaAtras.setText("Segundos Restantes: " + millisUntilFinished / 1000)
+                }
+                override fun onFinish() {
+                    val notificacion=RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+                    val r= getRingtone(activity,notificacion)
+                    r.play()
+                    this.cancel()
+                }
+            }.start()
+        }
+
+
     }
 
     override fun onDestroy() {
@@ -36,5 +57,30 @@ class SprintsFragment : Fragment() {
         activity?.supportActionBar?.title = "Entrenar"
         super.onDestroy()
     }
+
+
+
+    fun play(view: View){
+//        var txtTiempo=mBinding.tvCuentaAtras.text.toString().toLong()
+//        var tvCuentaAtras=mBinding.tvCuentaAtras
+//        var tiempoSegundos= txtTiempo.toString().toLong()
+//        var tiempoMilisegundos=tiempoSegundos*1000
+//        object : CountDownTimer(tiempoMilisegundos,1000){
+//            override fun onFinish() {
+//                val notificacion=RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+//                val r= getRingtone(activity,notificacion)
+//                r.play()
+//                this.cancel()
+//            }
+//            override fun onTick(millisUntilFinished: Long) {
+//                //val tiempoSegundos=(millisUntilFinished/1000).toInt()+1
+//                tvCuentaAtras?.text="0"
+//            }
+//        }.start()
+
+
+
+    }
+
 
 }
--- App1/app/src/main/java/com/example/runimprove/TrainingActivity.kt
+++ App1/app/src/main/java/com/example/runimprove/TrainingActivity.kt
@@ -3,6 +3,7 @@ package com.example.runimprove
 import android.R
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment

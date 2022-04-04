caa33d168c7d90ffccb28b9f44501c91666213b8
AlejandroEscalona <1alejandroescalona@gmail.com>
2022-04-04 19:27:44 +0200

    Redistribution in sprint fragment

--- App1/app/src/main/java/com/example/runimprove/SprintsFragment.kt
+++ App1/app/src/main/java/com/example/runimprove/SprintsFragment.kt
@@ -1,5 +1,10 @@
 package com.example.runimprove
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import android.hardware.TriggerEvent
+import android.hardware.TriggerEventListener
 import android.media.RingtoneManager
 import android.media.RingtoneManager.getRingtone
 import android.os.Bundle
@@ -11,13 +16,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.runimprove.databinding.FragmentSprintsBinding
-
+import androidx.core.content.ContextCompat.getSystemService
 
 class SprintsFragment : Fragment() {
 
-    private lateinit var mBinding : FragmentSprintsBinding
 
+    private lateinit var mBinding : FragmentSprintsBinding
+    private lateinit var sensorManager: SensorManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
@@ -31,30 +38,62 @@ class SprintsFragment : Fragment() {
         val activity = activity as? TrainingActivity
         //activity?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
         activity?.supportActionBar?.title = "Entrenar Sprints"
+       /* val enlace: TextView = mBinding.twEnalce
+        enlace.movementMethod = LinkMovementMethod.getInstance()*/
 
-        val enlace: TextView = mBinding.twEnalce
-        enlace.movementMethod = LinkMovementMethod.getInstance()
+
+
+        this.sensorManager = requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.let {
+            var accelerometer = it
+        }
+
 
         mBinding.btnPlay.setOnClickListener {
-            object : CountDownTimer(10000, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
-                    mBinding.tvCuentaAtras.setText("Segundos Restantes: " + millisUntilFinished / 1000)
-                    mBinding.btnPause.setOnClickListener{
-                        mBinding.tvCuentaAtras.setText("")
-                        cancel()
+                object : CountDownTimer(10000, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        mBinding.tvCuentaAtras.setText("" + millisUntilFinished / 1000)
+                        mBinding.btnPause.setOnClickListener {
+                            mBinding.tvCuentaAtras.setText("")
+                            cancel()
+                        }
                     }
-                }
-                override fun onFinish() {
-                    val notificacion=RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
-                    val r= getRingtone(activity,notificacion)
-                    r.play()
-                    this.cancel()
-                    mBinding.btnStopMusic.setOnClickListener{
-                        r.stop()
+                    override fun onFinish() {
+                        val notificacion =
+                            RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+                        val r = getRingtone(activity, notificacion)
+                        r.play()
+                        this.cancel()
+                        mBinding.btnStopMusic.setOnClickListener {
+                            r.stop()
+                        }
                     }
-                }
-            }.start()
-        }
+                }.start()
+            }
+
+
+            mBinding.btnPlay2.setOnClickListener {
+                object : CountDownTimer(120000, 1000) {
+                    override fun onTick(millisUntilFinished: Long) {
+                        mBinding.tvCuentaAtras2.setText("" + millisUntilFinished / 1000)
+                        mBinding.btnPause.setOnClickListener {
+                            mBinding.tvCuentaAtras2.setText("")
+                            cancel()
+                        }
+                    }
+
+                    override fun onFinish() {
+                        val notificacion =
+                            RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+                        val r = getRingtone(activity, notificacion)
+                        r.play()
+                        this.cancel()
+                        mBinding.btnStopMusic.setOnClickListener {
+                            r.stop()
+                        }
+                    }
+                }.start()
+            }
 
 
     }

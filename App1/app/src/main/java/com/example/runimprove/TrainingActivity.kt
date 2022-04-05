fbbfb1caa2f96769b4d7b1d9fc9698be93144142
AlejandroEscalona <1alejandroescalona@gmail.com>
2022-04-05 19:50:44 +0200

    Scroll sprints

--- App1/app/src/main/java/com/example/runimprove/TrainingActivity.kt
+++ App1/app/src/main/java/com/example/runimprove/TrainingActivity.kt
@@ -1,17 +1,14 @@
 package com.example.runimprove
 
-import android.R
-import android.content.Intent
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.View
 import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
 import com.example.runimprove.databinding.ActivityTrainingBinding
-
 
 
 class TrainingActivity : AppCompatActivity() {

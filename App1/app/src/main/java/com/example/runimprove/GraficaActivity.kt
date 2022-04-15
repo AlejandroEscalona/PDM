b1b10611276688142ed99b1bc99bffd6e96845bb
AlejandroEscalona <1alejandroescalona@gmail.com>
2022-04-15 14:59:06 +0200

    Grafico terminado

--- App1/app/src/main/java/com/example/runimprove/GraficaActivity.kt
+++ App1/app/src/main/java/com/example/runimprove/GraficaActivity.kt
@@ -33,10 +33,10 @@ class GraficaActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowHomeEnabled(true);
 
         pieChart = findViewById(R.id.PieChart)
-        initPieChart()
+        pieChart.setEntryLabelTextSize(22f)
 
+        initPieChart()
         setDataToPieChart()
-        //crearGrafico()
     }
 
     private fun initPieChart() {
@@ -47,36 +47,44 @@ class GraficaActivity : AppCompatActivity() {
         pieChart.setTouchEnabled(false)
         pieChart.setDrawEntryLabels(false)
         //adding padding
-        pieChart.setExtraOffsets(20f, 0f, 20f, 20f)
+        pieChart.setExtraOffsets(20f, 0f, 20f, 0f)
         pieChart.setUsePercentValues(true)
         pieChart.isRotationEnabled = false
         pieChart.setDrawEntryLabels(false)
-        pieChart.legend.orientation = Legend.LegendOrientation.VERTICAL
+        pieChart.legend.orientation = Legend.LegendOrientation.HORIZONTAL
         pieChart.legend.isWordWrapEnabled = true
+        pieChart.legend.textSize = 36f
+        pieChart.legend.formSize = 30f
 
     }
 
     private fun setDataToPieChart() {
+        val datos = database.stadistic()
         pieChart.setUsePercentValues(true)
         val dataEntries = ArrayList<PieEntry>()
-        dataEntries.add(PieEntry(72f, "Android"))
-        dataEntries.add(PieEntry(26f, "Ios"))
-        dataEntries.add(PieEntry(2f, "Other"))
+        dataEntries.add(PieEntry(datos[0].toFloat(), getString(R.string.sprints)))
+        dataEntries.add(PieEntry(datos[1].toFloat(), getString(R.string.hit)))
+        dataEntries.add(PieEntry(datos[2].toFloat(), getString(R.string.bajadas)))
+        dataEntries.add(PieEntry(datos[3].toFloat(), getString(R.string.resistencia)))
+        dataEntries.add(PieEntry(datos[4].toFloat(), getString(R.string.tecnica)))
+        dataEntries.add(PieEntry(datos[5].toFloat(), getString(R.string.estiramientos)))
 
         val colors: ArrayList<Int> = ArrayList()
         colors.add(Color.parseColor("#4DD0E1"))
         colors.add(Color.parseColor("#FFF176"))
         colors.add(Color.parseColor("#FF8A65"))
+        colors.add(Color.parseColor("#8bf6ff"))
+        colors.add(Color.parseColor("#ffc4ff"))
+        colors.add(Color.parseColor("#98ee99"))
 
         val dataSet = PieDataSet(dataEntries, "")
         val data = PieData(dataSet)
-
         // In Percentage
         data.setValueFormatter(PercentFormatter())
-        dataSet.sliceSpace = 3f
+        dataSet.sliceSpace = 6f
         dataSet.colors = colors
         pieChart.data = data
-        data.setValueTextSize(15f)
+        data.setValueTextSize(20f)
         pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
        // pieChart.animateY(1400, Easing.EaseInOutQuad)
 
@@ -85,11 +93,11 @@ class GraficaActivity : AppCompatActivity() {
         pieChart.transparentCircleRadius = 61f
         pieChart.isDrawHoleEnabled = true
         pieChart.setHoleColor(Color.WHITE)
-
+        pieChart.setCenterTextSize(22f)
 
         //add text in center
         pieChart.setDrawCenterText(true);
-        pieChart.centerText = "Mobile OS Market share"
+        pieChart.centerText = "Porcentaje por tipos de Entreno"
 
 
 

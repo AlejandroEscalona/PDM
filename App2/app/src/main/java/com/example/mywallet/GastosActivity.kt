bd6f83b336ce80b69bd75088afaf13f6b3fe6cee
AlejandroEscalona <1alejandroescalona@gmail.com>
2022-05-02 06:18:39 +0200

    Form to insert bills

--- App2/app/src/main/java/com/example/mywallet/GastosActivity.kt
+++ App2/app/src/main/java/com/example/mywallet/GastosActivity.kt
@@ -2,6 +2,7 @@ package com.example.mywallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.mywallet.databinding.ActivityGastosBinding
 
 class GastosActivity : AppCompatActivity() {
@@ -16,6 +17,10 @@ class GastosActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
         supportActionBar?.setDisplayShowHomeEnabled(true);
+
+        val categorias = resources.getStringArray(R.array.gastosList)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, categorias)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
     }
 
     override fun onSupportNavigateUp(): Boolean {
